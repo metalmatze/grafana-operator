5bc922244673171590268e1c7d273ed64f9b9c96
Matthias Loibl <mail@matthiasloibl.com>
2019-10-22 00:40:08 +0200

    Run http requests against Grafana adding and removing dashboards

--- pkg/controller/common/grafana_client.go
+++ pkg/controller/common/grafana_client.go
@@ -3,6 +3,7 @@ package common
 import (
 	"bytes"
 	"crypto/md5"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -10,7 +11,6 @@ import (
 	"net/url"
 
 	"github.com/buger/jsonparser"
-
 	"github.com/integr8ly/grafana-operator/pkg/apis/integreatly/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
@@ -31,13 +31,23 @@ func (g *GrafanaClient) IsKnown(kind string, o runtime.Object) (bool, error) {
 		d := o.(*v1alpha1.GrafanaDashboard)
 		uid := dashboardUID(d)
 
-		resp, err := http.Get(g.api.String() + "/api/dashboards/uid/" + uid)
+		req, err := http.NewRequest(http.MethodGet, g.api.String()+"/api/dashboards/uid/"+uid, nil)
+		if err != nil {
+			return false, err
+		}
+		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("Authorization", "Bearer eyJrIjoiQ0VLSTZ0TXR6ZjlBNllUMm5FOGc2TWgySXUzVnVZYk0iLCJuIjoib3BlcmF0b3IiLCJpZCI6MX0=")
+
+		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
 			return false, err
 		}
 		if resp.StatusCode/100 == 2 {
 			return true, nil
 		}
+
+		// TODO: Compare versions of dashboard
+
 		return false, nil
 	default:
 		return false, errors.New(fmt.Sprintf("unknown kind: %v", kind))
@@ -48,22 +58,22 @@ func dashboardUID(d *v1alpha1.GrafanaDashboard) string {
 	return fmt.Sprintf("%x", md5.Sum([]byte(d.Namespace+d.Name)))
 }
 
-func (g *GrafanaClient) UpdateDashboard(d *v1alpha1.GrafanaDashboard, json string) (bool, error) {
+func (g *GrafanaClient) UpdateDashboard(d *v1alpha1.GrafanaDashboard, j string) (bool, error) {
 	uid := dashboardUID(d)
 
-	jsonb, err := jsonparser.Set([]byte(json), []byte(fmt.Sprintf(`"%s"`, uid)), "uid")
+	jsonb, err := jsonparser.Set([]byte(j), []byte(fmt.Sprintf(`"%s"`, uid)), "uid")
 	if err != nil {
 		return false, err
 	}
 
-	jsonb = []byte(fmt.Sprintf(`{"dashboard": %s}`, string(jsonb)))
+	body := []byte(fmt.Sprintf(`{"dashboard": %s, "overwrite": true}`, string(jsonb)))
 
-	body := bytes.NewBuffer(jsonb)
-	req, err := http.NewRequest(http.MethodPost, g.api.String()+"/api/dashboards/db", body)
+	req, err := http.NewRequest(http.MethodPost, g.api.String()+"/api/dashboards/db", bytes.NewBuffer(body))
 	if err != nil {
 		return false, err
 	}
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer eyJrIjoiQ0VLSTZ0TXR6ZjlBNllUMm5FOGc2TWgySXUzVnVZYk0iLCJuIjoib3BlcmF0b3IiLCJpZCI6MX0=")
 
 	resp, err := http.DefaultClient.Do(req) // TODO: Don't use DefaultClient
 	if err != nil {
@@ -75,14 +85,50 @@ func (g *GrafanaClient) UpdateDashboard(d *v1alpha1.GrafanaDashboard, json strin
 		return true, nil
 	}
 
-	log.Info("failed updating dashboard", "status", resp.Status, "namespace", d.Namespace, "name", d.Name)
+	defer resp.Body.Close()
 
-	respbody, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(respbody))
+	var respPayload struct {
+		Message string `json:"message"`
+	}
 
-	return false, nil
+	err = json.NewDecoder(resp.Body).Decode(&respPayload)
+	if err != nil {
+		// TODO
+	}
+
+	log.Info("failed updating dashboard",
+		"status", resp.Status,
+		"namespace", d.Namespace,
+		"name", d.Name,
+		"response", respPayload.Message,
+	)
+
+	return false, errors.New(respPayload.Message)
 }
 
 func (g *GrafanaClient) DeleteDashboard(d *v1alpha1.GrafanaDashboard) error {
-	panic("implement me")
+	uid := dashboardUID(d)
+
+	req, err := http.NewRequest(http.MethodDelete, g.api.String()+"/api/dashboards/uid/"+uid, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer eyJrIjoiQ0VLSTZ0TXR6ZjlBNllUMm5FOGc2TWgySXUzVnVZYk0iLCJuIjoib3BlcmF0b3IiLCJpZCI6MX0=")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode/100 == 2 {
+		log.Info("successfully deleted dashboard", d.Namespace, d.Name)
+		return nil
+	}
+
+	defer resp.Body.Close()
+	respbody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(respbody))
+
+	return errors.New(fmt.Sprintf("unexpected response: %s", resp.Status))
 }
